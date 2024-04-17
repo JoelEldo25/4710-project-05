998fb0e990f4c2ad626493e2b4b2fe9a403b7630
JoelEldo25 <xxq5gd@virginia.edu>
2024-04-17 12:28:29 -0400

    Testing and training loops set up

--- nn.py
+++ nn.py
@@ -17,3 +17,31 @@ class SleepNetwork(nn.Module):
         x = self.flatten(x)
         logits = self.layers(x)
         return logits
+
+def train(dataloader, model, loss_function, optimizer):
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        pred = model(X)
+        loss = loss_function(pred, y)
+
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        if batch % 100 == 0:
+            print(f"loss: {loss:>7f}  Batch {batch:>5d}")
+
+def test(dataloader, model, loss_fn):
+    model.eval()
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
