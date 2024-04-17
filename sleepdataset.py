d471e0aef0acf275444819e27334f24c8b6b9d17
JoelEldo25 <xxq5gd@virginia.edu>
2024-04-17 12:46:53 -0400

    Downloaded a dataset, implemented dataset class

--- sleepdataset.py
+++ sleepdataset.py
@@ -3,8 +3,16 @@ from torch.utils.data import Dataset
 
 
 class SleepDataset(Dataset):
-    def __init__(self, file):
-        reader = csv.reader(open('data.csv'))
+    def __init__(self, filename):
+        reader = csv.reader(open(filename))
+        self.data = []
         for row in reader:
-            print(row)
+            self.data.append(row)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        attributes = self.data[index][:5] + self.data[index][6:]
+        return attributes, self.data[index][5]
 
