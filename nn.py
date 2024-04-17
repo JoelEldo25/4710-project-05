17a0eeefcf8d44561895ed43aed6fdd15e17eff0
JoelEldo25 <xxq5gd@virginia.edu>
2024-04-17 14:08:41 -0400

    Modified sleep dataset to properly store data as tensors, modified nn.py output to match what we are working on

--- nn.py
+++ nn.py
@@ -3,15 +3,23 @@ from torch import nn
 from sleepdataset import SleepDataset
 from torch.utils.data import DataLoader
 
+batch_size=64
+learning_rate=0.0001
+epochs=100
+
 dataset = SleepDataset("ss.csv")
+train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
 class SleepNetwork(nn.Module):
     def __init__(self):
+        super().__init__()
         self.flatten = nn.Flatten()
         self.layers = nn.Sequential(
-            nn.Linear(6,10),
+            nn.Linear(5,10),
             nn.ReLU(),
             nn.Linear(10,10),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Linear(10,1),
         )
 
@@ -29,8 +37,7 @@ def train(dataloader, model, loss_function, optimizer):
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        if batch % 100 == 0:
-            print(f"loss: {loss:>7f}  Batch {batch:>5d}")
+        print(f"loss: {loss:>7f}  Batch {batch:>5d}")
 
 def test(dataloader, model, loss_fn):
     model.eval()
@@ -42,8 +49,17 @@ def test(dataloader, model, loss_fn):
         for X, y in dataloader:
             pred = model(X)
             test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
 
     test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    print(f"Test Error: Avg loss: {test_loss:>8f} \n")
+
+
+model = SleepNetwork()
+loss_fn = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
--- sleepdataset.py
+++ sleepdataset.py
@@ -1,21 +1,28 @@
 import csv
-from torch.utils.data import Dataset
 
+import torch
+from torch.utils.data import Dataset
+from torch import tensor
 
 class SleepDataset(Dataset):
     def __init__(self, filename):
         reader = csv.reader(open(filename))
         self.data = []
+        self.targets = []
         next(reader)
         for row in reader:
-            datum = [row[2]] + row[4:8] + row[10:12]
+            datum = [row[2]] + [row[4]] + row[6:8] + [row[10]]
+            self.targets.append([float(row[5])])
             for i in range(len(datum)):
                 datum[i] = float(datum[i])
             self.data.append(datum)
+        self.data = tensor(self.data)
+        self.targets = tensor(self.targets)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        attributes = self.data[index][:2] + self.data[index][3:]
-        return attributes, self.data[index][2]
+        return self.data[index], self.targets[index]
+
+
